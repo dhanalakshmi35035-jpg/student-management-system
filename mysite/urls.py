feab8c4a7c86f80912950e8b49b6c88562dea4cc
dhanalakshmi35035-jpg <dhanalakshmi35035@gmail.com>
2026-06-26 16:25:32 +0530

    Updated attendance feature

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import  path,include
+from django.urls import path, include
 from polls.views import dashboard
 
 urlpatterns = [
-     path('', dashboard, name='dashboard'),
+    path('', dashboard, name='dashboard'),
     path('admin/', admin.site.urls),
-    path('polls/',include('polls.urls')),
+    path('polls/', include('polls.urls')),
 ]
 
